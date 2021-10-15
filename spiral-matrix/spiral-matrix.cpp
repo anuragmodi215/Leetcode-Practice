d8c61f0ae56e6862d48e2b9328e83a54634ac6cb
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2021-10-16 02:00:35 +0530

    Time: 3 ms (25.66%), Space: 6.9 MB (73.05%) - LeetHub

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& a) {
-        int row = a.size();
-        int cols = a[0].size();
-        
-        int left = 0;
-        int right = cols-1;
-        int top = 0;
-        int bottom = row-1;
-        int d=0;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int top,left,right,bottom;
+        top=0;
+        left=0;
+        right=n-1;
+        bottom=m-1;
         vector<int>v;
-        while(left<=right and top <= bottom){
-            if(d==0){
-            for(int i=left; i<=right; i++){
-                v.push_back(a[top][i]);
-
+        int i=0,j=0;
+        int k=1;
+        while(left<=right and top<=bottom){
+            if(k==1){
+               for(int i=left; i<=right; i++)
+                   v.push_back(matrix[top][i]);
+                top++; k=2;
             }
-                top++;
-                d=1;
+            else if(k==2){
+                for(int i=top; i<=bottom; i++)
+                   v.push_back(matrix[i][right]);
+                right--; k=3;
+                
+            } 
+            else if(k==3){
+              for(int i=right; i>=left; i--)
+                  v.push_back(matrix[bottom][i]);
+                  bottom--;k=4;
             }
-            else if(d==1){
-            for(int i=top; i<=bottom; i++){
-                v.push_back(a[i][right]);
-            }
-                 right--;
-                d=2;
-            }
-            else if(d==2){
-            for(int i = right; i>=left; i--){
-                v.push_back(a[bottom][i]);           
-            }
-                 bottom--;
-                d=3;
-            }
-            else if(d==3){
-            for(int i=bottom; i>=top;i--){
-                v.push_back(a[i][left]);             
-            }
-                left++;
-                d=0;
+            else if(k==4){
+               for(int i=bottom; i>=top; i--)
+                   v.push_back(matrix[i][left]);
+                left++; k=1;
             }
         }
         return v;
