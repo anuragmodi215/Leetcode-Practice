6d0232aadf4fd10ebb7a745a4355735f91dd131c
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2021-11-16 01:23:23 +0530

    Time: 12 ms (97.37%), Space: 18.6 MB (7.05%) - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v(n+1,-1);
+        for(int i=0; i<n; i++){
+            v[nums[i]]=nums[i];
+        }
+        for(int i=0; i<n+1; i++){
+            if(v[i]<0) return i;
+        }
+        return -1;
+    }
+};
