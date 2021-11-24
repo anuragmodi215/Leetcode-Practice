8dcc189e42b01d3e503cba78ee90c252258673b9
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2021-11-24 18:38:56 +0530

    Time: 44 ms (25.00%), Space: 19.6 MB (17.16%) - LeetHub

--- interval-list-intersections/interval-list-intersections.cpp
+++ interval-list-intersections/interval-list-intersections.cpp
@@ -6,6 +6,7 @@ public:
         vector<vector<int>> ans;
         if(n1==0 or n2==0) return ans;
         int i=0,j=0;
+        
         while(i<n1 and j<n2){
             int strt=max(firstList[i][0],secondList[j][0]);
             int ending=min(firstList[i][1],secondList[j][1]);
