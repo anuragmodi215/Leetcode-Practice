ed142b37b5a3bbe4dc9ee3388a06536f9163a0fc
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2021-12-11 16:50:22 +0530

    Time: 8 ms (69.25%), Space: 13.7 MB (17.37%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    int firstOccurence(vector<int>a,int n, int x){
-        int strt=0,end =n-1,res=-1;
-        int mid;
-        while(strt<=end){
-            mid = strt+((end-strt)/2);
-            if(a[mid]==x){
-                res=mid; end=mid-1;
-            }
-            else if(x<a[mid])end=mid-1;
-            else strt=mid+1;
-        }
-        return res;
-    }
-    int lastOccurence(vector<int>a,int n, int x){
-        int strt=0,end =n-1,res=-1;
-        int mid;
-        while(strt<=end){
-            mid = strt+((end-strt)/2);
-            if(a[mid]==x){
-                res=mid; strt=mid+1;
-            }
-            else if(x<a[mid])end=mid-1;
-            else strt=mid+1;
-        }
-        return res;
-    }
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>v;
-        int n = nums.size();
-        if(n==0){
-            v.push_back(-1);
-            v.push_back(-1);
-            return v;
+        int strt=0,end=nums.size()-1;
+        vector<int>ans;
+        int res=-1;
+        while(strt<=end){
+            int mid=strt+(end-strt)/2;
+            if(nums[mid]==target){
+                res=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target) strt=mid+1;
+            else end=mid-1;
         }
-        int first=firstOccurence(nums,n,target);
-        int last=lastOccurence(nums,n,target);
-        v.push_back(first);
-        v.push_back(last);
-        return v;
+        
+        ans.push_back(res);
+        
+        strt=0,end=nums.size()-1,res=-1;
+        
+        while(strt<=end){
+            int mid=strt+(end-strt)/2;
+            if(nums[mid]==target){
+                res=mid;
+                strt=mid+1;
+            }
+            else if(nums[mid]<target) strt=mid+1;
+            else end=mid-1;
+        }
+       
+        ans.push_back(res);
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
