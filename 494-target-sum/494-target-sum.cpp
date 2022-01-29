d0f2801b06eca9b460fe779e79fdf64e7fd764e2
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-01-30 00:38:27 +0530

    Time: 12 ms (78.55%), Space: 12.5 MB (40.04%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -27,10 +27,21 @@ public:
         for(auto i:nums) arr_sum+=i;
         int sumi=(arr_sum+target)/2;
         
-        // if(nums.size()==1){
-        //     if(nums[0]==abs(target)) return 1;
-        // }
+      
         if(arr_sum<abs(target) or (arr_sum+target)%2==1) return 0;
         return helper(nums,nums.size(),sumi);
     }
 };
+
+
+/*
+First Condition :
+If the total sum of the array is less than the target sum, then in no any way we can get the target sum.
+Second Condition :
+Basically here we are splitting array into two subarrays. One having + sign and other having - sign. Let the sum of first subarray be S1 and sum of second array be S2.
+S1 + S2 = Sum(sum of the array)
+S1 - S2 = Target
+2S1 = Sum + Target
+S1 = (Sum + Target)/2
+Thus, sum + target should be an even number.
+*/
