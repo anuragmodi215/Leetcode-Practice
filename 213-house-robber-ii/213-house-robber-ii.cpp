3aaaf2cc038f5ee1ebd308212ee14d00aec29f52
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-02-03 23:13:18 +0530

    Time: 8 ms (6.27%), Space: 8 MB (16.01%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-//     int helper(vector<int>& nums, int n){
-//         if(n<0) return 0;
-         
-//         int x=max((helper(nums,n-2)+nums[n]),helper(nums,n-1));
-//         //int y=helper(nums,n-1,true);
-//         return x;
-//     }
     int solve(vector<int>& nums){
         int n=nums.size();
         vector<int>dp(n);
@@ -24,14 +17,13 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
+        
         vector<int>a;
         for(int i=0; i<n-1; i++) a.push_back(nums[i]);
+        
         vector<int>b;
         for(int i=1; i<n; i++) b.push_back(nums[i]);
+        
         return max(solve(a),solve(b));
-        
-        
-        //return max((helper(a,a.size()-1)),(helper(b,b.size()-1)));
-        //return helper(nums,nums.size()-1,true);
     }
 };
