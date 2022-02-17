93eaf55bfbca7271e54ce05b7720d7d151c88413
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-02-18 00:17:40 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (46.53%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-//     int helper(vector<int>& nums,int n){
-//         if(n<0) return 0;
-        
-//         int sum=max((helper(nums,n-2)+nums[n]), helper(nums,n-1));
-//         return sum;
-//     }
+    // int maxi=INT_MIN;
+    // void helper(vector<int>& nums, int n, int sum){
+    //     if(n<0){
+    //         maxi=max(maxi,sum);
+    //         return;
+    //     }
+    //     helper(nums,n-2,sum+nums[n]);
+    //     helper(nums,n-1,sum);
+    // }
     int rob(vector<int>& nums) {
-        int n=nums.size();
+        //maxi=INT_MIN;
+        int n =nums.size();
+        // helper(nums,n-1,0);
+        // return maxi;
+        
         vector<int>dp(n);
         dp[0]=nums[0];
-        
-        for(int i=1; i<n; i++){
-            if(i>1)
-                dp[i]=max((dp[i-2]+nums[i]),dp[i-1]);
-            else
-                dp[i]=max(dp[i-1],nums[i]);
+        if(n>1)
+            dp[1]=max(nums[0],nums[1]);
+        if(n==2){
+            return max(dp[0],dp[1]);
         }
-        //return helper(nums,nums.size()-1);
+        for(int i=2; i<n; i++){
+            dp[i]=max((dp[i-2]+nums[i]),dp[i-1]);
+        }
         return dp[n-1];
     }
 };
