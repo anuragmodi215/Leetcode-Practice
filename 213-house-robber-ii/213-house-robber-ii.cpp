f1532a6b2325f47e2f67bf89e19c700633095c45
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-02-18 01:00:38 +0530

    Time: 0 ms (100.00%), Space: 8 MB (16.83%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-    int solve(vector<int>& nums){
-        int n=nums.size();
+    int helper(vector<int>& nums, int n){
+        if(n==0) return 0;
+        
         vector<int>dp(n);
         dp[0]=nums[0];
         for(int i=1; i<n; i++){
-            if(i>1){
-                dp[i]=max((dp[i-2]+nums[i]),(dp[i-1]));
+            if(i-2>=0){
+                dp[i]=max((dp[i-2]+nums[i]),dp[i-1]);
             }
             else{
                 dp[i]=max(dp[i-1],nums[i]);
-            }           
+            }
         }
+        
         return dp[n-1];
     }
     int rob(vector<int>& nums) {
+        vector<int>a;
+        vector<int>b;
         int n=nums.size();
         if(n==1) return nums[0];
+        for(int i=0; i<n-1; i++)
+            a.push_back(nums[i]);
         
-        vector<int>a;
-        for(int i=0; i<n-1; i++) a.push_back(nums[i]);
-        
-        vector<int>b;
-        for(int i=1; i<n; i++) b.push_back(nums[i]);
-        
-        return max(solve(a),solve(b));
+        for(int i=1; i<n; i++){
+            b.push_back(nums[i]);
+        }
+        int x=helper(a,n-1);
+        int y=helper(b,n-1);
+        return max(x,y);
     }
 };
