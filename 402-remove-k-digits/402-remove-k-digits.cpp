fc89950596fbee3d39f4688564d3bf8c9e0552e1
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-02-19 02:14:48 +0530

    Time: 4 ms (83.57%), Space: 7.1 MB (35.38%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     string removeKdigits(string nums, int k) {
-        stack<char>st;
         int n=nums.size();
-        
+        stack<char>st;
         for(int i=0; i<n; i++){
-            if(st.empty()){
-                st.push(nums[i]);
+            while(!st.empty() and k>0 and st.top()>nums[i]){
+                st.pop();
+                k--;
             }
-            else{
-                while(k>0 and !st.empty() and st.top()>nums[i]){
-                    st.pop();
-                    k--;
-                    
-                }
-                 st.push(nums[i]);
-            }
+            st.push(nums[i]);
         }
+        
         while(k>0){
             st.pop();
             k--;
         }
-        //cout<<st.top();
+        bool containsOnlyZeros=true;;
         string ans="";
         while(!st.empty()){
-            //if(st.top()!='0')
-            ans.push_back(st.top());
+            ans+=st.top();
+            if(st.top()!='0'){
+                containsOnlyZeros=false;
+            }
             st.pop();
         }
-        //cout<<ans;
-        if(ans.size()==0) return "0";
-
+        if(containsOnlyZeros) return "0";
         reverse(ans.begin(),ans.end());
-        int ansLen=ans.size();
         int i;
-        if(ans.size()>1){
-            for(i=0; i<ansLen; i++){
+        int ansLen=ans.size();
+        for(i=0; i<ansLen; i++){
             if(ans[i]=='0') continue;
             else break;
         }
-        ans = ans.substr(i);
-        }
-        if(ans.size()==0) return "0";
+        ans=ans.substr(i);
         return ans;
     }
 };
+
+
+
+
+
+
+
+
