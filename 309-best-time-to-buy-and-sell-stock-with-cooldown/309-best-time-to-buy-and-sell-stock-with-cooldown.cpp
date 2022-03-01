f4bbc204f1d09a3e35e6aa594e8ecd1d4979164a
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-03-02 01:32:15 +0530

    Time: 8 ms (45.78%), Space: 12.2 MB (13.07%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -3,8 +3,10 @@ public:
     int helper(vector<int>& prices, int n, int i, bool canBuy,vector<vector<int>>&dp){
         if(i>=n) return 0;
         if(dp[i][canBuy]!=-1) return dp[i][canBuy];
+        
         int &ans=dp[i][canBuy];
-        int ans1=helper(prices,n,i+1,canBuy,dp);
+        
+        
         int ans2=0;
         if(canBuy){
             ans2=-prices[i]+helper(prices,n,i+1,false,dp);
@@ -12,13 +14,12 @@ public:
         else{
             ans2=prices[i]+helper(prices,n,i+2,true,dp);
         }
+        int ans1=helper(prices,n,i+1,canBuy,dp);
         return ans = max(ans1,ans2);
     }
     int maxProfit(vector<int>& prices) {
         vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
         
         return helper(prices,prices.size(),0,true,dp);
-        
-        //return dp[prices.size()][]
     }
 };
