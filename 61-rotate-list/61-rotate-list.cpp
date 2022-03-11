b2a5081f7d8e6488a17c5b5dd0472d514170f197
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-03-12 00:13:25 +0530

    Time: 12 ms (42.92%), Space: 11.7 MB (88.88%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* Reverse(ListNode*curr,ListNode*prev,ListNode*next){
+        while(curr){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr=next;
+        }
+        return prev;
+    }
+    int lengthOfLinkedList(ListNode* head){
+        ListNode* ptr = head;
+        int n = 0;
+        while(ptr){
+            n++;
+            ptr=ptr->next;
+        }
+        return n;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head or !head->next or k==0) return head;
+        int n = lengthOfLinkedList(head);
+        
+        if(n==k) return head;
+        k=k%n;
+        if(k==0) return head;
+        ListNode* dummy = Reverse(head,NULL,NULL);
+        ListNode* ptr = dummy;
+        
+        cout<<n<<endl;
+        
+        for(int i=1; i<k; i++){
+           ptr=ptr->next; 
+        }
+        ListNode* head1 = dummy;
+        ListNode* head2 = ptr->next;
+        cout<<head2->val;
+        ptr->next = NULL;
+        
+        ListNode* first = Reverse(head1,NULL,NULL);
+        ListNode* second = Reverse(head2,NULL,NULL);
+        
+        ptr = first;
+        while(ptr->next){
+            ptr=ptr->next;
+        }
+        ptr->next = second;
+        return first;
+    }
+};
