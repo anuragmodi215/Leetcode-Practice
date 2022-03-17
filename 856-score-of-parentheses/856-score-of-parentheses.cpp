6c1b5aa054ea7176c251643fc3660c8a3858eb95
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-03-18 03:06:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (83.00%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        stack<int>stack;
+        //stack<int>stack;
         int n=s.size();
         int score=0;
+        int cnt=0;
         for(int i=0; i<n; i++){
             if(s[i]=='('){
-                stack.push(score);
-                score=0;
+                cnt++;
             }
             else{
-                score=stack.top()+max(score*2,1);
-                stack.pop();
+                cnt--;
+                if(s[i-1]=='('){
+                    score=score+pow(2,cnt);
+                }
             }
         }
         return score;
