9614bf6dc41153c3999125d95c7d1bcb67d8014b
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-03-19 18:02:19 +0530

    Time: 3 ms (73.57%), Space: 7 MB (14.11%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         int n=s.size();
+        vector<int>visited(26);
         unordered_map<char,int>freq;
-        string ans="";
-        vector<int>visited(26,0);
         stack<char>st;
-        for(int i=0; i<n ;i++){
+        string ans="";
+        for(int i=0; i<n; i++){
             freq[s[i]]++;
         }
         for(int i=0; i<n; i++){
             freq[s[i]]--;
             if(!visited[s[i]-'a']){
-                while(!st.empty() and st.top()>s[i] and freq[st.top()]>0){
-                    visited[st.top()-'a']=0;
-                    st.pop();
+                 while(!st.empty() and st.top()>s[i] and freq[st.top()]>0){
+                        visited[st.top()-'a']=0;
+                        st.pop();
                 }
                 st.push(s[i]);
                 visited[s[i]-'a']=1;
             }
-            
+           
         }
-        
         while(!st.empty()){
             ans=st.top()+ans;
             st.pop();
