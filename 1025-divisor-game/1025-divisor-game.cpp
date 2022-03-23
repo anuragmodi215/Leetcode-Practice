3cbb294268b31534ce3deb9285564e459c2940ec
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-03-23 22:38:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.80%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
-    int helper(int n,vector<int>&dp){
-       if(n==1) return dp[n]= 0;
-       if(dp[n]!=-1) return dp[n];
-        for(int i=1; i<n; i++){
-            if(n%i==0){
-               if(helper(n-i,dp)==0) return dp[n]= 1; 
-            }
-        }
-        return dp[n]=0;
-    }
     bool divisorGame(int n) {
-        vector<int>dp(n+1,-1);
-        helper(n,dp);
-        return dp[n];
+        //if our remainder is 1 that means Alice has no more moves
+        return n%2==0;
     }
 };
