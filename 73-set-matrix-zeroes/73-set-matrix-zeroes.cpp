32f63cad7d41fe3537e4991577b9779719cdc0f4
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-04-27 16:53:51 +0530

    Time: 16 ms (71.82%), Space: 13.4 MB (19.60%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    void MakeItZero(vector<vector<int>>& matrix, int n, int m, pair<int,int> ind){
-        int i=ind.first;
-        int j=ind.second;
-        int forward,backward;
-        forward=backward=i;
-        while(forward<n or backward>=0){
-            if(forward<n) matrix[forward++][j]=0;
-            if(backward>=0) matrix[backward--][j]=0;
+    void helper(vector<vector<int>>& matrix,vector<pair<int,int>>&zeros,int n, int m){
+        int zerosSize = zeros.size();
+        for(int k=0; k<zerosSize; k++){
+            int row = zeros[k].first;
+            int col = zeros[k].second;
+            
+            for(int i=row; i<n; i++){
+                matrix[i][col] = 0;
+            }
+            for(int i=row; i>=0; i--){
+                matrix[i][col] = 0;
+            }
+        
+            for(int j=col; j<m; j++)
+                matrix[row][j] = 0;
+        
+            for(int j=col; j>=0; j--)
+                matrix[row][j] = 0;
         }
-        forward=backward=j;
-        while(forward<m or backward>=0){
-            if(forward<m) matrix[i][forward++]=0;
-            if(backward>=0) matrix[i][backward--]=0;
-        }
+
     }
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        set<pair<int,int>>set;
-        pair<int,int>index;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<pair<int,int>> zeros;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
-                    index.first=i;
-                    index.second=j;
-                    set.insert(index);
+                    zeros.push_back({i,j});
                 }
             }
         }
-        
-        for(auto i:set){
-            pair<int,int>ind=i;
-            MakeItZero(matrix,rows,cols,ind);
-        }
+        helper(matrix,zeros,n,m);
     }
 };
