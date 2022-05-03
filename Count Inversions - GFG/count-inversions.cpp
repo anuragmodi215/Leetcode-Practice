45035d548f616d615989127f21cdec186b06e971
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-03 14:23:48 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -9,20 +9,19 @@ class Solution{
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
+    
     long long int merge(long long arr[], long long temp[], int low, int mid, int high){
+        int i=low;
+        int j=mid; 
+        int k=low;
         long long int inv = 0;
-        int i,j,k;
-        i=low;
-        j=mid;
-        k=low;
-        //cout<<j<<endl;
         while(i<mid and j<=high){
             if(arr[i]<=arr[j]){
                 temp[k++] = arr[i++];
             }
             else{
                 temp[k++] = arr[j++];
-                inv = inv + (mid-i);
+                inv = inv + mid-i;
             }
         }
         
@@ -32,19 +31,18 @@ class Solution{
         while(j<=high){
             temp[k++] = arr[j++];
         }
-        for(int i = low; i<=high; i++){
+        for(int i=low; i<=high; i++){
             arr[i] = temp[i];
         }
         return inv;
     }
     long long int mergeSort(long long arr[], long long temp[], int low, int high){
         long long int inv = 0;
-        
         if(low<high){
             int mid = low+(high-low)/2;
-            inv = inv + mergeSort(arr, temp, low, mid);
-            inv = inv + mergeSort(arr, temp, mid+1,high);
-            inv = inv + merge(arr, temp, low, mid+1, high);
+            inv = inv + mergeSort(arr,temp, low, mid);
+            inv = inv + mergeSort(arr, temp, mid+1, high);
+            inv = inv + merge(arr,temp,low, mid+1, high);
         }
         return inv;
     }
@@ -54,6 +52,7 @@ class Solution{
         long long temp[n];
         return mergeSort(arr,temp,0,n-1);
     }
+
 };
 
 // { Driver Code Starts.
