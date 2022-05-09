0139380df18cf233e16118dc7135053837ec9a94
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-10 00:04:46 +0530

    Time: 44 ms (90.15%), Space: 14.6 MB (74.13%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,20 +8,61 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*>mp;
-        ListNode* ptr = headA;
-        while(ptr){
-            mp.insert(ptr);
-            ptr = ptr->next;
+    ListNode * traverse(ListNode* temp, int times){
+        for(int i=0; i<times; i++){
+            temp=temp->next;
         }
-        ptr = headB;
-        while(ptr){
-            if(mp.find(ptr)!=mp.end()){
-                return ptr;
-            }
-            ptr = ptr->next;
+        return temp;
+    }
+    
+    ListNode * checkForIntersection(ListNode * headA, ListNode * headB){
+        while(headA and headB){
+            if(headA==headB) return headA;
+            headA = headA->next;
+            headB = headB->next;
         }
         return NULL;
     }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode*first = headA;
+        ListNode*second = headB;
+        
+        int firstLength=0;
+        int secondLength=0;
+        
+        while(first or second){
+            if(first){
+                firstLength++;
+                first=first->next;
+            }
+            if(second){
+                secondLength++;
+                second = second->next;
+            }
+        }
+        
+        int difference=0;
+        ListNode* temp = NULL;
+        if(firstLength>secondLength){
+            difference = firstLength-secondLength;
+            temp = headA;
+            temp = traverse(temp,difference);
+            return checkForIntersection(temp,headB);
+        }
+        else{
+            difference = secondLength-firstLength;
+            temp = headB;
+            temp = traverse(temp,difference);
+            return checkForIntersection(temp,headA);
+        }
+        return NULL;
+        
+    }
 };
+
+
+
+
+
+
+
