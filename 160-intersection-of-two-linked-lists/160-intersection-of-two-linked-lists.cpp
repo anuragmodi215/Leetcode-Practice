c9f7c278001e59c20726a54f196967c23edbe3c9
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-10 00:39:53 +0530

    Time: 74 ms (32.19%), Space: 14.6 MB (32.98%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,61 +8,17 @@
  */
 class Solution {
 public:
-    ListNode * traverse(ListNode* temp, int times){
-        for(int i=0; i<times; i++){
-            temp=temp->next;
-        }
-        return temp;
-    }
-    
-    ListNode * checkForIntersection(ListNode * headA, ListNode * headB){
-        while(headA and headB){
-            if(headA==headB) return headA;
-            headA = headA->next;
-            headB = headB->next;
-        }
-        return NULL;
-    }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode*first = headA;
-        ListNode*second = headB;
-        
-        int firstLength=0;
-        int secondLength=0;
-        
-        while(first or second){
-            if(first){
-                firstLength++;
-                first=first->next;
-            }
-            if(second){
-                secondLength++;
-                second = second->next;
-            }
-        }
-        
-        int difference=0;
-        ListNode* temp = NULL;
-        if(firstLength>secondLength){
-            difference = firstLength-secondLength;
-            temp = headA;
-            temp = traverse(temp,difference);
-            return checkForIntersection(temp,headB);
-        }
-        else{
-            difference = secondLength-firstLength;
-            temp = headB;
-            temp = traverse(temp,difference);
-            return checkForIntersection(temp,headA);
+        ListNode* first = headA;
+        ListNode* second = headB;
+        while(first and second){
+            if(first==second) return first;
+            first = first->next;
+            second = second->next;
+            if(first==second) return first;
+            if(first==NULL) first = headB;
+            if(second==NULL) second = headA;
         }
         return NULL;
-        
     }
 };
-
-
-
-
-
-
-
