7c96f0f19960b841f7dc35f0a4dccca0f1f90c12
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-14 03:24:12 +0530

    Time: 17 ms (33.94%), Space: 7.2 MB (69.10%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,51 +1,59 @@
 class Solution {
 public:
-    bool isValid(int row, int col, vector<string>board, int n){
-        int r=row;
-        int c=col;
+    
+    bool isSafe(vector<string>&board, int row, int col, int n){
+        //cout<<row<<" "<<col<<endl;
+        int fRow,fCol,bRow,bCol;
+         //fRow = row;
+         fCol = col;
+         bRow = row;
+         bCol = col;
+        while(bRow>=0 and bCol>=0){
+            if(board[bRow][bCol]=='Q') return false;
+            bRow--;bCol--;
+        }
+         bRow = row;
+         bCol = col;
         
-        for(int i=c; i>=0; i--){
-            if(board[r][i]=='Q') return false;
+        for(int i=bCol; i>=0; i--){
+            if(board[row][i]=='Q') return false;
         }
-           
-           // while(c>=0){
-           //     if(board[r][c]=='Q') return false;
-           //     c--;
-           // }
-           // c=col;
-        while(r>=0 and c>=0){
-            if(board[r][c]=='Q') return false;
-            r--;c--;
+        
+        for(int i=bRow; i>=0 ; i--){
+            if(board[i][col]=='Q') return false;
         }
-        r=row;
-        c=col;
-        while(r<n and c>=0){
-            if(board[r][c]=='Q') return false;
-            r++;c--;
+        while(row<n and col>=0){
+            if(board[row][col]=='Q') return false;
+            row++;col--;
         }
         return true;
     }
-    void helper(int n,vector<string>&board,vector<vector<string>>&ans,int col){
+    
+    void solve(int col, int n, vector<string>&board,vector<vector<string>>&ans)
+    {
         if(col==n){
             ans.push_back(board);
             return;
         }
-        for(int row=0;row<n; row++){
-            if(isValid(row,col,board,n)){
-                board[row][col]='Q';
-                helper(n,board,ans,col+1);
-                board[row][col]='.';
+        for(int row = 0; row<n; row++){
+            if(isSafe(board,row,col,n)){
+                board[row][col] = 'Q';
+                solve(col+1, n, board, ans);
+                board[row][col] = '.';
             }
         }
     }
+    
+    
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string>board(n);
         string s(n,'.');
-        for(int i=0; i<n; i++){
-            board[i]=s;
+        vector<string>board(n);
+        vector<vector<string>>ans;
+        for(int i=0;  i<n; i++){
+            board[i] = s;
         }
-        helper(n,board,ans,0);
+        //cout<<"funCall"<<endl;
+        solve(0,n,board,ans);
         return ans;
     }
 };
