719f8e406f24e26bc5d27ce25d5569ec2c97629c
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-14 01:33:33 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (38.26%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    void helper(int ind, int n,vector<int>nums, vector<vector<int>>&ans){
+        if(ind==n){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=ind; i<n; i++){
+            swap(nums[i],nums[ind]);
+            helper(ind+1, n, nums,ans);
+            swap(nums[i],nums[ind]);
+            
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        //vector<int>v;
+        int n = nums.size();
+        helper(0,n,nums,ans);
+        return ans;
+    }
+};
