0e2afb1ac46f867df55c07bda403d29cf71f4789
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-14 21:28:56 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,46 +10,35 @@ using namespace std;
 
 class Solution{
     public:
-    void helper(vector<vector<int>> &m,int end, string s, vector<vector<int>>&visited,vector<string>&ans,int row, int col){
-        if(row==end-1 and col==end-1){
+    vector<vector<int>>dir{
+        {1,0},{0,-1},{0,1},{-1,0}
+    };
+    vector<char>path{'D','L','R','U'};
+    void helper(vector<vector<int>> &m, int n, int row, int col, string s, vector<string>&ans,vector<vector<int>>&visited){
+        //cout<<row<<" "<<col<<endl;
+        if(col==n-1 and row==n-1){
             ans.push_back(s);
             return;
         }
-        if(row+1<end and m[row+1][col]==1 and !visited[row+1][col]){
-            s.push_back('D');
-            visited[row][col]=1;
-            helper(m,end,s,visited,ans,row+1,col);
-            s.pop_back();
-            visited[row][col]=0;
+        for(int i=0; i<4; i++){
+            int nx = row+dir[i][0];
+            int ny = col+dir[i][1];
+            if(nx<n and ny<n and nx>=0 and ny>=0 and !visited[nx][ny] and m[nx][ny]==1){
+                visited[row][col]=1;
+                helper(m,n,nx,ny,s+path[i],ans,visited);
+                visited[row][col]=0;
+            }
         }
-        if(col-1>=0 and m[row][col-1]==1 and !visited[row][col-1]){
-            s.push_back('L');
-            visited[row][col]=1;
-            helper(m,end,s,visited,ans,row,col-1);
-            s.pop_back();
-            visited[row][col]=0;
-        }
-        if(col+1<end and m[row][col+1]==1 and !visited[row][col+1]){
-            s.push_back('R');
-            visited[row][col]=1;
-            helper(m,end,s,visited,ans,row,col+1);
-            s.pop_back();
-            visited[row][col]=0;
-        }
-        if(row-1>=0 and m[row-1][col]==1 and !visited[row-1][col]){
-            s.push_back('U');
-            visited[row][col]=1;
-            helper(m,end,s,visited,ans,row-1,col);
-            s.pop_back();
-            visited[row][col]=0;
-        }
+        
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
+        vector<string> ans;
         vector<vector<int>> visited(n,vector<int>(n,0));
-        vector<string>ans;
-        if(m[0][0]==1)
-        helper(m,n,"",visited,ans,0,0);
+        if(m[0][0]==1){
+            helper(m,n,0,0,"",ans,visited);
+        }
+        
         return ans;
     }
 };
