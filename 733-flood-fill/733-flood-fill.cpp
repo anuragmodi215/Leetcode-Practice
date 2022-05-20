c3af1919687de2c3990c6be9c7a19e8dd12e76c5
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-20 22:09:47 +0530

    Time: 17 ms (27.06%), Space: 14.2 MB (37.87%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,-1,0,1};
+    bool isValid(int i, int j, vector<vector<int>>& image, vector<vector<int>>&visited, int colour,int newColor){
+        int n = image.size();
+        int m = image[0].size();
+        if(i<n and j<m and i>=0 and j>=0 and !visited[i][j] and image[i][j]==colour)return true;
+        return false;
+    }
+    void dfs(int i, int j, vector<vector<int>>& image, vector<vector<int>>&visited, int colour,int newColor){
+        for(int k=0; k<4; k++){
+            int x = dx[k]+i;
+            int y = dy[k]+j;
+            if(isValid(x,y,image,visited,colour,newColor)){
+                visited[x][y]=1;
+                dfs(x,y,image,visited,colour,newColor);
+                image[x][y]=newColor;
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        dfs(sr,sc,image,visited,image[sr][sc],newColor);
+        image[sr][sc] = newColor;
+        return image;
+    }
+};
