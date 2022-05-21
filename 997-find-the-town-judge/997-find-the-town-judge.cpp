4fe0c2a5e05d5565e98edcb82e9d678f440fc948
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-21 22:53:39 +0530

    Time: 265 ms (33.05%), Space: 61.5 MB (56.77%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.cpp
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,int>inDegree;
-        unordered_map<int,int>outDegree;
+        unordered_map<int,int>inOut;
         int m = trust.size();
-        if(n==1 and m==0) return 1;
+        if(m==0 and n==1)return 1;
         for(int i=0; i<m; i++){
-            inDegree[trust[i][1]]++;
-            outDegree[trust[i][0]]++;
+            inOut[trust[i][1]]++;
+            inOut[trust[i][0]]--;
         }
-        for(auto in: inDegree){
-            if(in.second==n-1){
-                if(outDegree.find(in.first)==outDegree.end()){
-                    return in.first;
-                }
-            }
+        for(auto i:inOut){
+            if(i.second==n-1) return i.first;
         }
         return -1;
     }
