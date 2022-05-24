bd84513b3a2024e19594da8a9bf4ead960b644c0
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-25 01:44:47 +0530

    Time: 3 ms (55.80%), Space: 7.3 MB (56.77%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
-        int odd_cnt=0;
-        int even_cnt=0;
+//         since cost of moving a coin +2 or -2 is Zero therfore we will
+//         either move even coins or odd coins to either even position or odd position
+//
+//          take all coins to 1 3 5 7 9 or all coins to 0 2 4 6 8
+        
+//         either take all coins to odd position or all coins to even position
+//         then the cost of +2 move or -2 move is 0
+        
+        int even=0,odd=0;
         for(auto i:position){
-            if(i%2==0)even_cnt++;
-            else odd_cnt++;
+            if(i%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
         }
-        return min(even_cnt,odd_cnt);
+        return min(odd,even);
     }
 };
