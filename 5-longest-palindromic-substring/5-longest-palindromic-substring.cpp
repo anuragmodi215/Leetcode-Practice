de50a5f6fcea5e54df9e7211d68f41fa1b6cbd9f
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-25 22:20:50 +0530

    Time: 19 ms (90.73%), Space: 7.1 MB (81.83%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int strt=0; int length = 0;
-    void expandFromCenter(int i, int j, string &s, int n){
-        int tempLen=0;
-        while(i>=0 and j<n){
-            if(s[i]==s[j]){
-                tempLen = j-i+1;
-                i--;j++;
-            }
-            else break;
+    void expandFromCenter(int firstIndex, int secondIndex, string &s, int &start, int &length, int n){
+        int currLen = 0;
+        int i = firstIndex;
+        int j = secondIndex;
+        while(i>=0 and j<n and s[i]==s[j]){
+            i--;
+            j++;
         }
-         if(tempLen>length){
-             strt=i+1;
-             length = tempLen;
-         }
+        i=i+1;
+        j=j-1;
+        if(length<j-i+1){
+            start = i;
+            length = j-i+1;
+        }
     }
     string longestPalindrome(string s) {
         int n = s.size();
-        strt=0;
-        length = 0;
+        int start = 0;
+        int length = 0;
         for(int i=0; i<n; i++){
-            expandFromCenter(i,i,s,n);
-            expandFromCenter(i,i+1,s,n);
+            expandFromCenter(i,i,s,start,length,n);
+            expandFromCenter(i,i+1,s,start,length,n);
         }
-        return s.substr(strt,length);
+        return s.substr(start,length);
     }
 };
