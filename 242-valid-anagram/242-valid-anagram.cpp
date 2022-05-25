09c7ea958a1ed6aa8c551025f48bbfddfb05a888
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-26 02:19:45 +0530

    Time: 4 ms (94.40%), Space: 7.3 MB (46.68%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n1=s.size();
-        int n2=t.size();
-        if(n1!=n2) return false;
-        unordered_map<char,int>mp;
-        for(int i=0; i<n1; i++){
-            mp[s[i]]++;
-        }
-        for(int i=0; i<n2; i++){
-            mp[t[i]]--;
-            if(mp[t[i]]<0) return false;
-        }
-        // if(mp.size()==0) return true;
-        // else return false;
-        return true;
+        int dp[26]={0};
+        int biggiLength = s.size();
+        int simmiLength = t.size();
+        if(biggiLength<simmiLength) return false;
         
+        for(int i=0; i<biggiLength; i++){
+            dp[s[i]-'a']++;
+        }
+        for(int i=0; i<simmiLength; i++){
+            dp[t[i]-'a']--;
+            if(dp[t[i]-'a']<0){
+                return false;
+            }
+        }
+        for(int i=0; i<26; i++){
+            if(dp[i]!=0) return false; 
+        }
+        return true;
     }
 };
