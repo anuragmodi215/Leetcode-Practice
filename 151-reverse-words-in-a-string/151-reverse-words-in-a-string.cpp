6d8983abe464e03ca0a3e284b0e07e8902202f18
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-05-25 22:06:18 +0530

    Time: 8 ms (51.96%), Space: 20.8 MB (24.79%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    string getWord(string &s, int i){
-        int j=i;
-        int n = s.size();
-        while(j<n and s[j] != ' '){
+    string getWord(int i, int n, string &s){
+        int j = i;
+        while(j<n and s[j]!=' '){
             j++;
         }
-        string temp = s.substr(i,j-i);
-        return temp;
+        return s.substr(i,j-i);
     }
     string reverseWords(string s) {
         string ans = "";
         int n = s.size();
         for(int i=0; i<n; i++){
-            if(s[i]==' ')continue;
-            string word = getWord(s,i);
-            cout<<word<<endl;
-            if(ans==""){
-                ans = word;
+            if(s[i]==' ') continue;
+            string word = getWord(i,n,s);
+            if(ans.size()==0){
+                ans = word+ans;
             }
             else{
                 ans = word+" "+ans;
             }
-            
             i = i+word.size();
         }
         return ans;
