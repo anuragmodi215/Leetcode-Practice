9d70017e27508a6613dcede9335869e1a0ce7e69
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-02 00:58:19 +0530

    Time: 5 ms (17.54%), Space: 6.2 MB (80.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -3,27 +3,21 @@ public:
     bool isValid(string s) {
         stack<char>st;
         int n = s.size();
-        if(n%2==1) return false;
-        
-        for(int i=0; i<n; i++){
-            //if(st.empty() and s[i]==')' or s[i]=='}' or s[i]==']') return false;
-            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
-                st.push(s[i]);
+        for(auto i:s){
+            if(i=='(' or i=='{' or i=='['){
+                st.push(i);
             }
-            else
-            {
+            else{
                 if(!st.empty()){
-                if(st.top()=='(' and s[i]==')') st.pop();
-                else if(st.top()=='{' and s[i]=='}')st.pop();
-                else if(st.top()=='[' and s[i]==']')st.pop();
-                else return false;
+                    if(st.top()=='(' and i==')') st.pop();
+                    else if(st.top()=='{' and i=='}') st.pop();
+                    else if(st.top()=='[' and i==']') st.pop();
+                    else return false;
                 }
                 else return false;
             }
-            
         }
-    
-        if(st.empty()) return true;
-        return false;
+        if(st.empty()) return 1;
+        return 0;
     }
 };
