442b1513a3b26b75a7d5e410229e660ea08096fc
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-04 22:54:52 +0530

    Time: 7 ms (75.47%), Space: 7.2 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
-     
-    void solve(int col,int n,vector<string>&board,vector<vector<string>>&ans,
-               vector<int>&left,vector<int>&lowerDiagonal,vector<int>&upperDiagonal)
-    {
+    bool isSafe(int row, int col, int n,vector<string>&board ){
+        int drow=row,dcol=col;
+        int urow=row,ucol=col;
+        
+        while(urow>=0 and ucol>=0){
+            if(board[urow][ucol]=='Q'){
+                return false;
+            }
+            urow--;ucol--;
+        }
+        urow = row, urow = col;
+        while(drow<n and dcol>=0){
+            if(board[drow][dcol]=='Q'){
+                return false;
+            }
+            drow++;dcol--;
+        }
+        for(int j=col; j>=0; j--){
+            if(board[row][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    void helper(int col, int n, vector<string>&board, vector<vector<string>>&ans){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row = 0; row<n; row++){
-            if(left[row]==0 and lowerDiagonal[row+col]==0 and upperDiagonal[(n-1)+(col-row)]==0){
+            if(isSafe(row,col,n,board)){
                 board[row][col] = 'Q';
-                left[row]=1;
-                lowerDiagonal[row+col] = 1;
-                upperDiagonal[n-1+col-row] = 1;
-                
-                solve(col+1, n, board, ans, left, lowerDiagonal, upperDiagonal);
-                
-                board[row][col] = '.';
-                left[row]=0;
-                lowerDiagonal[row+col] = 0;
-                upperDiagonal[n-1+col-row] = 0;
+                helper(col+1,n,board,ans);
+                board[row][col]='.';
             }
         }
     }
     
-    
     vector<vector<string>> solveNQueens(int n) {
+     
         string s(n,'.');
-        vector<string>board(n);
+        vector<string>board(n,s);
         vector<vector<string>>ans;
-        vector<int>left(n);
-        vector<int>lowerDiagonal(2*n-1);
-        vector<int>upperDiagonal(2*n-1);
-        for(int i=0;  i<n; i++){
-            board[i] = s;
-        }
-        //cout<<"funCall"<<endl;
-        solve(0,n,board,ans,left,lowerDiagonal,upperDiagonal);
+        helper(0,n,board,ans);
         return ans;
+        
     }
 };
