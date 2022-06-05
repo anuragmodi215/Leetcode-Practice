242f07f9e71f8eeb37101d373cc86bdf1ece8135
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-06 01:43:59 +0530

    Time: 42 ms (69.89%), Space: 13.4 MB (29.79%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    int dx[4] = {1,0,-1,0};
-    int dy[4] = {0,1,0,-1};
-    bool isValid(int i, int j, vector<vector<char>>& grid){
+    int dx[4]={1,0,-1,0};
+    int dy[4]={0,1,0,-1};
+    bool isValid(vector<vector<char>>& grid,vector<vector<int>>&visited, int i, int j){
         int n = grid.size();
         int m = grid[0].size();
-        if(i>=0 and j>=0 and i<n and j<m and grid[i][j]=='1')
-        {
-            return true;
-        }
+        if(i<n and j<m and i>=0 and j>=0 and grid[i][j]=='1' and !visited[i][j]) return true;
         return false;
     }
-    void dfs(int i, int j, vector<vector<char>>& grid){
-        grid[i][j]='0';
+    int dfs(vector<vector<char>>& grid,vector<vector<int>>&visited, int i, int j){
         for(int k=0; k<4; k++){
-            int x = i+dx[k];
-            int y = j+dy[k];
-            if(isValid(x,y,grid)){
-                dfs(x,y,grid);
+            visited[i][j]=1;
+            int newX=i+dx[k];
+            int newY=j+dy[k];
+            if(isValid(grid,visited,newX,newY)){
+                dfs(grid,visited,newX,newY);
             }
         }
+        return 1;
     }
+    
     int numIslands(vector<vector<char>>& grid) {
+        
         int n = grid.size();
         int m = grid[0].size();
-        int ans = 0;
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        int cnt=0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]=='1'){
-                    ans++;
-                    dfs(i,j,grid);
+                if(grid[i][j]=='1' and !visited[i][j]){
+                    cnt+=dfs(grid,visited,i,j);
                 }
             }
         }
-        return ans;
+        return cnt;
     }
 };
