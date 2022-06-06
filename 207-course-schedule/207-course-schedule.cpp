b57127118211acda65b5b93973ee229c40c362c3
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-07 04:05:28 +0530

    Time: 29 ms (57.32%), Space: 14.6 MB (32.15%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    bool dfs(int node, unordered_map<int,vector<int>>&adj, unordered_map<int,bool>&visited,unordered_map<int,bool>&dfsVisited){
+    bool dfs(int node,unordered_map<int,vector<int>>&adj,vector<int>&visited,vector<int>&dfsVisited){
         visited[node] = 1;
-        dfsVisited[node] = 1;
-        for(auto neighbour: adj[node]){
-            if(!visited[neighbour]){
-                bool x = dfs(neighbour,adj,visited,dfsVisited);
+        dfsVisited[node]=1;
+        for(int neig : adj[node]){
+            if(!visited[neig]){
+                bool x = dfs(neig, adj, visited, dfsVisited);
                 if(x) return true;
             }
             else{
-                if(dfsVisited[neighbour]) return true;
+                if(dfsVisited[neig]==1) return true;
             }
         }
         dfsVisited[node] = 0;
         return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) { 
         unordered_map<int,vector<int>>adj;
-        unordered_map<int,bool>visited;
-        unordered_map<int,bool>dfsVisited;
         int n = prerequisites.size();
+        if(n==0) return true;
+        vector<int>visited(numCourses+1);
+        vector<int>dfsVisited(numCourses+1);
         for(int i=0; i<n; i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
@@ -28,7 +30,7 @@ public:
         for(int i=0; i<numCourses; i++){
             if(!visited[i]){
                 bool x = dfs(i,adj,visited,dfsVisited);
-                if(x) return false;
+                if(x) return false; 
             }
         }
         return true;
