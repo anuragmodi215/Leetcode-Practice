5f76e8f0827c740c5fe05eec9844ca790505c22b
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-07 23:18:29 +0530

    Time: 3 ms (43.85%), Space: 5.7 MB (96.47%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    double helper(double x, int n){
+    double findPower(double x, int n){
         double result = 1;
         while(n>0){
-                if(n%2==1)
-                {
-                    result = result*x;
-                }
-                x = x*x;
-                n = n/2;
+            if(n%2==1){
+                result*=x;
             }
+            x*=x;
+            n/=2;
+        }
         return result;
     }
     double myPow(double x, int n) {
         double ans=0;
         if(n<0){
-            n = abs(n);
-            ans = helper(x,n);
+            ans = findPower(x,abs(n));
             ans = 1/ans;
         }
         else{
-            ans = helper(x,n);
+            ans = findPower(x,n);
         }
         return ans;
     }
