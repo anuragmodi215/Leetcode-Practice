c426aec4147f119be589fa991861f82c9702772c
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-08 17:40:28 +0530

    Time: 26 ms (35.09%), Space: 9.5 MB (50.08%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    bool canPermute(int start, int curr, vector<int>&nums){
+        for(int i=start; i<curr; i++){
+            if(nums[i]==nums[curr]) return false;
+        }
+        return true;
+    }
     void helper(int index,int n, vector<int>nums, vector<vector<int>>&ans){
         if(index==n){
             ans.push_back(nums);
             return;
         }
         for(int i = index; i<n; i++){
-            if(i!=index and nums[i]==nums[index]) continue;
+            if(i!=index and !canPermute(index,i,nums)) continue;
             swap(nums[i],nums[index]);
             helper(index+1,n,nums,ans);
-            //swap(nums[i],nums[index]);
+            swap(nums[i],nums[index]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
