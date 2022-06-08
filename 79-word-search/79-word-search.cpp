6d0f5d6bfa626a0e1e0164240baf00cfe68b357b
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-08 12:58:07 +0530

    Time: 1080 ms (10.20%), Space: 7.9 MB (74.95%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int dx[4]={1,0,-1,0};
-    int dy[4]={0,1,0,-1};
-    bool isValid(int x, int y, int index,vector<vector<char>>& board, string &word){
+    vector<pair<int,int>>dir{{1,0},{0,1},{-1,0},{0,-1}};
+    bool isValid(int i, int j, int index, vector<vector<char>>& board, string&word){
         int n = board.size();
         int m = board[0].size();
-        if(x>=0 and x<n and y>=0 and y<m  and word[index]==board[x][y]) 
+        if(i<n and j<m and i>=0 and j>=0 and board[i][j]==word[index]){
             return true;
-        else return false;
+        }
+        return false;
     }
-    bool dfs(int i, int j,int index,vector<vector<char>>& board,string &word){
-        if(index == word.size()) return true;
-        
+    bool dfs(int i, int j, int index,vector<vector<char>>& board,string &word){
+        if(index==word.size()) return true;
+
         for(int k=0; k<4; k++){
-            if(isValid(i,j,index,board,word)){
+            if(isValid(i,j,index,board,word)){  
                 board[i][j]='*';
-                int x = i+dx[k];
-                int y = j+dy[k];
-                bool validity = dfs(x,y,index+1,board,word);
+                int dx = i+dir[k].first;
+                int dy = j+dir[k].second;
+                bool validity = dfs(dx,dy,index+1,board,word);
                 if(validity) return true;
-                board[i][j] = word[index];
+                board[i][j]=word[index];
             }
         }
         return false;
@@ -27,7 +27,6 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        //vector<vector<int>>visited(n,vector<int>(m,0));
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 bool x = dfs(i,j,0,board,word);
