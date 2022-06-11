7ab2553156ba7a6ab03cd4db3972ba70894e3010
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-11 17:57:38 +0530

    Time: 2 ms (40.83%), Space: 6.3 MB (15.33%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int findUniqueStairsCount(int n , vector<int>&dp){
-        if(n==0 or n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        int oneStep = findUniqueStairsCount(n-1,dp);
-        int twoStep = findUniqueStairsCount(n-2,dp);
-        return dp[n] = oneStep+twoStep;
-    }
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return findUniqueStairsCount(n,dp);
-        //return dp[n];
+        if(n<=2) return n;
+        vector<int>dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
