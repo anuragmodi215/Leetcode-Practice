0104ac1d6a80ab016b1db6f92258a0c767dba1bd
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-15 01:35:34 +0530

    Time: 13 ms (86.49%), Space: 7.7 MB (61.19%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,81 +1,23 @@
 class Solution {
 public:
-    string helper(string s, string str, int n, int m){
-        vector<vector<int>>dp(n+1, vector<int>(m+1));
-        string sequence="";
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if(s[i-1]==str[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
-                }
+    int lcs(string X, string Y,int m,int n){
+        int dp[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
             }
         }
         
-        int i=n,j=m;
-        while(i>0 and j>0){
-            if(s[i-1]==str[j-1]){
-                sequence.push_back(s[i-1]);
-                i--;j--;
-            }
-            else{
-                if(dp[i-1][j]>dp[i][j-1]) i--;
-                else j--;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(X[i-1]==Y[j-1]) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
             }
         }
-        
-        reverse(sequence.begin(),sequence.end());
-        return sequence;
+        return dp[m][n];
     }
-    int minDistance(string s, string str) {
-        int n=s.size();
-        int m=str.size();
-        string sequence=helper(s,str,n,m);
-        cout<<sequence<<endl;
-        int i=0,j=0,k=0;
-        int o=sequence.size();
-        int cnt=0;
-        
-        while(i<n and j<m and k<o){
-            if(s[i]==str[j] and s[i]==sequence[k]){
-                i++;j++;k++;
-            }
-            else{
-                if(s[i]==sequence[k]){
-                    j++;
-                    cnt++;
-                }
-                else{
-                    i++; cnt++;
-                }
-            }
-        }
-        cnt=cnt+(n-i);
-        cnt=cnt+(m-j);
-        return cnt;
-        // while(i<n){
-        //     cnt++;
-        //     i++;
-        // }
-        // while(j<m){
-        //     cnt++;
-        //     j++;
-        // }
-        //return cnt;
+    int minDistance(string s1, string s2) {
+        int m = s1.size(), n = s2.size();
+        return m+n-2*lcs(s1,s2,m,n) ;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
