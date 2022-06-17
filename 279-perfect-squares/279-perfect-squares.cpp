84300e06eb383cb37cbb96896406ad03296ca786
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-17 23:54:55 +0530

    Time: 392 ms (25.85%), Space: 9.6 MB (31.14%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int helper(int n, vector<int>&dp){
-        if(n==0) return 0;
+        if(n<=0) return 0;
         if(dp[n]!=-1) return dp[n];
         int ans = INT_MAX;
         for(int i=1; i*i<=n; i++){
-            int x = 1 + helper(n-i*i, dp);
-            ans = min(x,ans);
+            int x = 1+helper(n-(i*i), dp);
+            ans = min(ans,x);
         }
-        dp[n] = ans;
-        return dp[n];
+        return dp[n] = ans;
     }
     int numSquares(int n) {
         vector<int>dp(n+1,-1);
