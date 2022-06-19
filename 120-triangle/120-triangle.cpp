b377f4149f5727b0439dfb6ca8ddc10889c41899
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-19 20:04:39 +0530

    Time: 16 ms (14.35%), Space: 9 MB (21.38%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& triangle,int row,int col,vector<vector<int>>&dp,int n){
-        if(col>=n) return 0;
-        if(row==n){
-            // return triangle[row][col];
-            return 0;
-        }
-        if(dp[row][col]!=-1) return dp[row][col];
-        int down = triangle[row][col]+helper(triangle,row+1,col,dp,n);
-        int downRight = triangle[row][col]+helper(triangle,row+1,col+1,dp,n);
-        return dp[row][col] = min(down,downRight);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
+        
         int n = triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-        return helper(triangle,0,0,dp,n);
+        
+        for(int i=0; i<n; i++){
+            dp[n-1][i] = triangle[n-1][i];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=i; j>=0 ; j--){
+                int top = triangle[i][j]+dp[i+1][j];
+                int topRight = triangle[i][j]+dp[i+1][j+1];
+                dp[i][j] = min(top,topRight);
+            }
+        }
+        return (dp[0][0]);
     }
 };
