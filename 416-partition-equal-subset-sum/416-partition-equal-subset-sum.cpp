16d413a0ab871e3775c59d1db7e71828bc392026
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-21 01:37:33 +0530

    Time: 477 ms (21.14%), Space: 76 MB (12.47%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    bool helper(vector<int>& nums,int sum,int n){
-        // if(sum==0) return;
-        // if(n==0) return;
-        vector<vector<bool>>dp(n+1,vector<bool>(sum+1,false));
-        
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<1; j++){
-                dp[i][j]=true;
-            }
+    bool helper(vector<int>&nums,int index, int target, vector<vector<int>>&dp){
+        if(target==0) return true;
+        if(index==0){
+            if(nums[0]==target) return true;
+            else return false;
         }
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<sum+1; j++){
-                if(j>=nums[i-1]){
-                    dp[i][j]=dp[i-1][j-nums[i-1]] or dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notTake = helper(nums,index-1,target,dp);
+        int take = false;
+        if(nums[index]<=target){
+            take = helper(nums,index-1,target-nums[index],dp);
         }
-        
-        return dp[n][sum];
+        return dp[index][target] = take or notTake;
     }
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(auto i:nums){
-            sum+=i;
-        }
+        int n = nums.size();
+        for(int i=0; i<n; i++) sum+=nums[i];
         if(sum%2==1) return false;
-        
-        return helper(nums,sum/2,nums.size());
+        sum=sum/2;
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        return helper(nums,n-1,sum,dp);
     }
 };
