dd7a51ab0b366a200af516ca9fdbb907862a6034
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-21 20:02:01 +0530

    Time: 154 ms (36.24%), Space: 45.7 MB (17.77%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,51 +1,27 @@
 class Solution {
 public:
-//     int helper(vector<int>& coins, int amount,int n){
-//         if(amount==0) return 0;
-//         if(n==0) return INT_MAX-1;
-        
-//         if(coins[n-1]<=amount){
-//              int take=1+helper(coins,amount-coins[n-1],n);
-//              int notTake=helper(coins,amount,n-1);
-           
-//             return min(take,notTake);
-//         }
-//         else{
-//             return helper(coins,amount,n-1);
-//         }
-//     }
-    
-    
-    int helper(vector<int>& coins, int amount,int n){
-       vector<vector<int>>dp(n+1, vector<int>(amount+1));
-       
-        for(int i=0; i<1; i++){
-            for(int j=1; j<amount+1; j++){
-                dp[i][j]=INT_MAX-1;
+    int helper(vector<int>& coins, int index, int amount,vector<vector<int>>&dp){
+        if(amount==0) return 0;
+        if(index==0){
+            if(amount%coins[index]==0){
+                return amount/coins[index];
             }
+            else return INT_MAX-1;
         }
-        
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<amount+1; j++){
-                if(coins[i-1]<=j){
-                    int take=1+dp[i][j-coins[i-1]];
-                    int notTake=dp[i-1][j];
-                    dp[i][j]=min(take,notTake);
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int notTake = 0+helper(coins,index-1,amount,dp);
+        int take = INT_MAX;
+        if(coins[index]<=amount){
+            take = 1+helper(coins,index,amount-coins[index],dp);
         }
-        return dp[n][amount];
+        return dp[index][amount] = min(notTake,take);
     }
-    
-     
-    
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        int x= helper(coins,amount,n);
-        if(x==INT_MAX-1) return -1;
-        else return x;
+        if(amount == 0) return 0;
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = helper(coins,n-1,amount,dp);
+        if(ans==INT_MAX-1) return -1;
+        return ans;
     }
 };
