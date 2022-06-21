ee3088a559ef29e777e4549c2f0d608892f65c7d
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-21 23:18:07 +0530

    Time: 18 ms (76.75%), Space: 18.5 MB (34.79%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-//     int solve(int amount, vector<int>& coins, int n){
-//         if(amount==0) return 1;
-//         if(n==0) return 0;
-        
-//         if(coins[n-1]<=amount){
-//             int take=solve(amount-coins[n-1],coins,n);
-//             int notTake=solve(amount, coins, n-1);
-//             return take+notTake;
-//         }
-//         else return solve(amount, coins, n-1);
-//     }
-    int solve(int amount, vector<int>& coins, int n){
-        vector<vector<int>>dp(n+1,vector<int>(amount+1));
-        int take=0,notTake=0;
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<1; j++){
-                dp[i][j]=1;
-            }
+    int helper(vector<int>&coins,int ind, int amount, vector<vector<int>>&dp){
+        if(amount==0) return 1;
+        if(ind==0){
+            if(amount%coins[0]==0) return 1;
+            else return 0;
         }
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<amount+1; j++){
-                if(coins[i-1]<=j){
-                    take=dp[i][j-coins[i-1]];
-                    notTake=dp[i-1][j];
-                    dp[i][j]=take+notTake;
-                }
-                else
-                dp[i][j]=dp[i-1][j];
-            }
+        if(dp[ind][amount]!=-1) return dp[ind][amount];
+        int notTake = helper(coins,ind-1, amount,dp);
+        int take = 0;
+        if(coins[ind]<=amount){
+            take = helper(coins,ind,amount-coins[ind],dp);
         }
-        return dp[n][amount];
+        return dp[ind][amount] = notTake+take;
     }
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        return solve(amount,coins,n);
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return helper(coins,n-1,amount,dp);
     }
 };
