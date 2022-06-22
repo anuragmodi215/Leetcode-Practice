9cff7b2fd71866f517c3da4e576f347d30ef50ec
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-22 19:21:34 +0530

    Time: 57 ms (22.93%), Space: 12.3 MB (17.75%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int lcs(string &s, string&t, int i, int j, vector<vector<int>>&dp){
-        if(i<0 or j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
+//     int lcs(string &s, string&t, int i, int j, vector<vector<int>>&dp){
+//         if(i<0 or j<0) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(s[i]==t[j]){
-            return dp[i][j] = 1+lcs(s,t,i-1,j-1,dp);
-        }
-        return dp[i][j] = max(lcs(s,t,i-1,j,dp),lcs(s,t,i,j-1,dp));
-    }
+//         if(s[i]==t[j]){
+//             return dp[i][j] = 1+lcs(s,t,i-1,j-1,dp);
+//         }
+//         return dp[i][j] = max(lcs(s,t,i-1,j,dp),lcs(s,t,i,j-1,dp));
+//     }
     int minDistance(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return m+n-2*(lcs(s,t,n-1,m-1,dp));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return m+n-2*(dp[n][m]);
     }
 };
