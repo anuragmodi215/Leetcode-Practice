9cec05ab111f6bae3a8c491ebf357128b05ada9e
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-22 16:43:25 +0530

    Time: 195 ms (40.61%), Space: 73.1 MB (7.43%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
-    string helper(string s, string str, int n, int m){
-        vector<vector<int>>dp(n+1,vector<int>(m+1));
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<m+1; j++){
-                if(s[i-1]==str[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+    int helper(string &s, string &t, int i, int j, vector<vector<int>>&dp){
+        if(i<0 or j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s[i]==t[j]){
+            return dp[i][j] = 1+helper(s,t,i-1,j-1,dp);
         }
-        
-        int i=n,j=m;
-        string seq="";
-        while(i>0 and j>0){
-            if(s[i-1]==str[j-1]){
-                seq.push_back(s[i]);
-                i--; j--;
-            }
-            else{
-                if(dp[i-1][j]>dp[i][j-1]) i--;
-                else j--;
-            }
-        }
-        //reverse(seq.begin(),seq.end());
-        return seq;
+
+        return dp[i][j]=max(helper(s,t,i-1,j,dp),helper(s,t,i,j-1,dp));
     }
     int longestPalindromeSubseq(string s) {
-        string str=s;
-        reverse(s.begin(),s.end());
-        cout<<s<<" "<<str<<endl;
-        int n=s.size();
-        int m=str.size();
-        string seq=helper(s,str,n,m);
-        cout<<seq<<endl;
-        return seq.size();
+        string t = s;
+        reverse(t.begin(),t.end());
+        int n = s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return helper(s,t,n-1,n-1,dp);
     }
 };
