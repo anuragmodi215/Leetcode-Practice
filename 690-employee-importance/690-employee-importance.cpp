fe80e7d158b7124c1f3b93010c4ebca286769636
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-06-24 17:25:56 +0530

    Time: 422 ms (7.73%), Space: 100.6 MB (6.34%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -10,22 +10,19 @@ public:
 
 class Solution {
 public:
+    void helper(int id, unordered_map<int,Employee*>mp,int&sum){
+        sum+=mp[id]->importance;
+        for(auto neig: mp[id]->subordinates){
+            helper(neig, mp, sum);
+        }
+    }
     int getImportance(vector<Employee*> employees, int id) {
-        queue<Employee*>q;
         unordered_map<int,Employee*>mp;
-        for(auto i:employees){
+        for(auto i: employees){
             mp[i->id] = i;
         }
-        q.push(mp[id]);
-        int imp=0;
-        while(!q.empty()){
-            auto front = q.front();
-            q.pop();
-            imp = imp + front->importance;
-            for(auto neig:front->subordinates){
-                q.push(mp[neig]);
-            }
-        }
-        return imp;
+        int sum=0;
+        helper(id,mp,sum);
+        return sum;
     }
 };
