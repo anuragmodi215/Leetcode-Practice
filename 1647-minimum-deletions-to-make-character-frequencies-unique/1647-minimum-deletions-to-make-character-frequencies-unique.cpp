465434880a036fffbaa578b2fecd552738c4c23f
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-01 00:25:55 +0530

    Time: 63 ms (80.95%), Space: 17.4 MB (54.83%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -3,18 +3,18 @@ public:
     int minDeletions(string s) {
         int n = s.size();
         vector<int>freq(26,0);
-        unordered_map<int,bool>mp;
-        int count=0;
         for(int i=0; i<n; i++){
             freq[s[i]-'a']++;
         }
+        unordered_map<int,bool>mp;
+        int count=0;
         for(int i=0; i<26; i++){
             if(freq[i]==0)continue;
             while(freq[i]>0 and mp.find(freq[i])!=mp.end()){
                 freq[i]--;
                 count++;
             }
-            mp[freq[i]]=1;
+            mp[freq[i]]=true;
         }
         return count;
     }
