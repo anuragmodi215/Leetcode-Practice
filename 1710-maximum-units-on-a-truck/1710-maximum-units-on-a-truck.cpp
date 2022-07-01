09ae77d4592a168b64920eb8e11ed061b89068c9
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-02 00:31:20 +0530

    Time: 57 ms (70.18%), Space: 16 MB (77.60%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    static bool myfun(vector<int>&a, vector<int>&b){
-        return a[1]>b[1];
+    static bool cmp(vector<int>&a, vector<int>&b){
+        if(a[1]==b[1]){
+            return a[0]<b[0];
+        }
+        return a[1]<b[1];
     }
-    int maximumUnits(vector<vector<int>>& box, int truckSize) {
-        sort(box.begin(),box.end(),myfun);
-        
-        int n=box.size();
-        int ans=0;
-        
-        for(int i=0; i<n; i++){
-            if(box[i][0]<=truckSize){
-                ans+=box[i][0]*box[i][1];
-                
-                truckSize-=box[i][0];
-                if(truckSize<=0)break;
+    
+    int maximumUnits(vector<vector<int>>& nums, int truckSize) {
+        sort(nums.begin(),nums.end(),cmp);
+        int n = nums.size()-1;
+        int ans = 0;
+        for(int i=n; i>=0; i--){
+            if(truckSize>=nums[i][0]){
+                ans+=nums[i][0]*nums[i][1];
+                truckSize=truckSize-nums[i][0];
             }
-            else {
-                ans+=truckSize*box[i][1];
-                truckSize-=box[i][0];
-                if(truckSize<=0)break;
+            else{
+                ans+=truckSize*nums[i][1];
+                break;
             }
         }
         return ans;
