77b2e3e13da647f353f46678af1255486947dbdb
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-02 12:25:18 +0530

    Time: 5 ms (27.11%), Space: 8.4 MB (23.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -6,12 +6,12 @@ public:
         int cnt1=0;
         int cnt2=0;
         for(int i=0; i<n; i++){
-            if(nums[i]==0) cnt0++;
-            else if(nums[i]==1) cnt1++;
+            if(nums[i]==0)cnt0++;
+            else if(nums[i]==1)cnt1++;
             else cnt2++;
         }
         int i=0;
-        for(i=0; i<cnt0; i++){
+        for(; i<cnt0; i++){
             nums[i]=0;
         }
         for(; i<cnt0+cnt1; i++){
@@ -20,5 +20,6 @@ public:
         for(; i<n; i++){
             nums[i]=2;
         }
+        //return nums;
     }
 };
