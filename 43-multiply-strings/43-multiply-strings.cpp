f21020aabb2225a03490b228480f45d2be93a505
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-14 14:15:28 +0530

    Time: 15 ms (43.71%), Space: 6.9 MB (43.62%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    string multiply(string num, string number) {
-        int rem=0;
-        string ans="";
-        int n=num.size(),m=number.size();
+    string multiply(string a, string b) {
+        if(a[0]=='0' or b[0]=='0') return "0";
+        int n = a.size();
+        int m = b.size();
         vector<int>v(n+m);
-        if(num=="0" or number=="0") return "0";
+        string ans ="";
         for(int i=n-1; i>=0; i--){
             for(int j=m-1; j>=0; j--){
-                v[i+j+1]+=(num[i]-'0')*(number[j]-'0');
-                v[i+j]+=v[i+j+1]/10;
-                v[i+j+1]%=10;
+                v[i+j+1]+=(a[i]-'0')*(b[j]-'0');
+                v[i+j]+=(v[i+j+1]/10);
+                v[i+j+1] = v[i+j+1]%10;
             }
         }
-        
         int i=0;
-        //string ans="";
-        while(i<n+m and v[i]==0) i++;
-        
-        for(int j=i; j<n+m; j++){
+        while(i<v.size() and v[i]==0)i++;
+       
+        for(int j=i; j<v.size(); j++){
             ans.push_back(v[j]+'0');
         }
         return ans;
