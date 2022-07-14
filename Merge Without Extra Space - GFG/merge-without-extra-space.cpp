d79e7b73a2c326f10822d2f4f1c47d978866ec20
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-14 11:26:46 +0530

    Added solution - LeetHub

---
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void merge(int small[], int large[], int n, int m) {
+	    // code here
+	   int i= n-1;
+	   int j=0;
+	   while(i>=0 and j<m){
+	       if(small[i]>large[j]){
+	           swap(small[i],large[j]);
+	       }
+	       i--;j++;
+	   }
+	   sort(small,small+n);
+	   sort(large,large+m);
+	}
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        int arr1[n], arr2[m];
+        for (i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+        for (i = 0; i < n; i++) {
+            cout << arr1[i] << " ";
+        }
+        for (i = 0; i < m; i++) {
+            cout << arr2[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
