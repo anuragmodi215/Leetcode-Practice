60a0d3c07382f073bc1a5ffb63108dbb3f8d341c
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-16 13:25:45 +0530

    Time: 1046 ms (23.02%), Space: 207.1 MB (30.04%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int helper(vector<int>& prices, int cap, bool buy,int ind,vector<vector<vector<int>>>&dp){
-        if(ind==prices.size() or cap==0) return 0;
-        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-        if(buy){
-            return dp[ind][buy][cap]=max(-prices[ind]+helper(prices,cap,0,ind+1,dp),helper(prices,cap,1,ind+1,dp));
-        }
-        else{
-            return dp[ind][buy][cap]=max(prices[ind]+helper(prices,cap-1,1,ind+1,dp), helper(prices,cap,buy,ind+1,dp));
-        }
-    }
+    // int helper(vector<int>& prices, int cap, bool buy,int ind,vector<vector<vector<int>>>&dp){
+    //     if(ind==prices.size() or cap==0) return 0;
+    //     if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+    //     if(buy){
+    //         return dp[ind][buy][cap]=max(-prices[ind]+helper(prices,cap,0,ind+1,dp),helper(prices,cap,1,ind+1,dp));
+    //     }
+    //     else{
+    //         return dp[ind][buy][cap]=max(prices[ind]+helper(prices,cap-1,1,ind+1,dp), helper(prices,cap,buy,ind+1,dp));
+    //     }
+    // }
     int maxProfit(vector<int>& prices) {
         int cap=2;
         bool buy=1;
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return helper(prices,cap,buy,0,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1; buy++){
+                for(int cap=1; cap<=2; cap++){
+                    if(buy){
+                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap]);
+                    }
+                    else{
+                        dp[ind][buy][cap] = max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+        
+        // return helper(prices,cap,buy,0,dp);
     }
 };
