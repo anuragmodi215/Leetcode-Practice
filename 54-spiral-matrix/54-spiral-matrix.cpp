309b7370a04f91aefa82f7d4e1daa8d610e7ac21
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-18 23:30:59 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.15%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -4,42 +4,40 @@ public:
         vector<int>v;
         int n = matrix.size();
         int m = matrix[0].size();
-        
-        int left = 0, top = 0;
-        int right = m-1, bottom = n-1;
-        int dir = 1;
+        int left=0,right=m-1,bottom=n-1,top=0;
+        int dir=1;
         while(left<=right and top<=bottom){
             if(dir==1){
                 for(int i=left; i<=right; i++){
                     v.push_back(matrix[top][i]);
                 }
                 top++;
-                dir = 2;
+                dir=2;
             }
-            
             else if(dir==2){
+                //cout<<top<<" "<<right<<endl;
                 for(int i=top; i<=bottom; i++){
+                    //cout<<matrix[i][right]<<" ";
                     v.push_back(matrix[i][right]);
                 }
                 right--;
-                dir = 3;
+                dir=3;
             }
             else if(dir==3){
                 for(int i=right; i>=left; i--){
                     v.push_back(matrix[bottom][i]);
                 }
-                 bottom--;
-                 dir = 4;
+                bottom--;
+                dir=4;
             }
             else if(dir==4){
                 for(int i=bottom; i>=top; i--){
                     v.push_back(matrix[i][left]);
                 }
-                 left++;
-                 dir = 1;
+                left++;
+                dir=1;
             }
         }
-        
         return v;
     }
 };
