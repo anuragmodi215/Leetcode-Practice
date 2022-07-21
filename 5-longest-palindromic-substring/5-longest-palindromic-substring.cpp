720085beea32a90c648e9f949dfd028d293d340a
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-21 12:07:12 +0530

    Time: 36 ms (77.30%), Space: 7.2 MB (82.07%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    void expandFromCenter(int firstIndex, int secondIndex, string &s, int &start, int &length, int n){
-        int currLen = 0;
-        int i = firstIndex;
-        int j = secondIndex;
-        while(i>=0 and j<n and s[i]==s[j]){
-            i--;
-            j++;
+    int left=0;
+    int right = 0;
+    int ans = 0;
+    void helper(int i, int j, string&s){
+        int n = s.size();
+        while(i>=0 and j<n){
+            if(s[i]==s[j]){
+                //cout<<s[i];
+                if(ans<j-i+1){
+                    ans = j-i+1;
+                    left = i;
+                    right = j;
+                }
+                i--;j++;
+            }
+            else break;
         }
-        i=i+1;
-        j=j-1;
-        if(length<j-i+1){
-            start = i;
-            length = j-i+1;
-        }
+        //return ans;
     }
     string longestPalindrome(string s) {
         int n = s.size();
-        int start = 0;
-        int length = 0;
-        for(int i=0; i<n; i++){
-            expandFromCenter(i,i,s,start,length,n);
-            expandFromCenter(i,i+1,s,start,length,n);
+        int ans = INT_MIN;
+        for(int i=0; i<n-1; i++){
+            helper(i,i,s);
+            helper(i,i+1,s);
         }
-        return s.substr(start,length);
+       
+        // cout<<left<<endl;
+        // cout<<right<<endl;
+        return s.substr(left,right-left+1);
     }
 };
