66a8b02ba3369978afcc70d69af01393944c2baf
Anurag Modi <87538645+anuragmodi215@users.noreply.github.com>
2022-07-22 13:05:50 +0530

    Time: 15 ms (71.69%), Space: 9 MB (56.92%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    int helper(string &a, string&b, int n, int m, vector<vector<int>>&dp){
-        if(n<0){
-            return m+1;
-        }
-        if(m<0){
-            return n+1;
-        }
-        if(n<0 and m<0){
-            return 0;
-        }
-        if(dp[n][m]!=-1) return dp[n][m];
-        if(a[n]==b[m]){
-            return dp[n][m]=helper(a,b,n-1,m-1,dp);
-        }
-        
-        else{
-            //replacement
-            int replacement = 1+helper(a,b,n-1,m-1,dp);
-            //insert
-            int insert = 1+helper(a,b,n,m-1,dp);
-            //delete
-            int deletion = 1+helper(a,b,n-1,m,dp);
-            return dp[n][m] = min(replacement,min(insert,deletion));
-        }
-        return 0;
-    }
+    
     int minDistance(string a, string b) {
         int n = a.size();
         int m = b.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return helper(a,b,n-1,m-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=1; i<=n; i++){
+            dp[i][0] = i;
+        }
+        for(int j=1; j<=m; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(a[i-1]==b[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int replacement = 1+dp[i-1][j-1];
+                    int insert = 1+dp[i][j-1];
+                    int deletion = 1+dp[i-1][j];
+                    dp[i][j] = min(replacement,min(insert,deletion));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
